89c97a7740afed3aa51278e8606c7723c7e950b8
Hanan Gershon Cohen <hanang.cohen@gmail.com>
2025-11-30 19:58:27 +0200

    Add UdpReceiver for UDP datagram listening
    Commented out NavigationManager instantiation and execution in Program.cs. Introduced UdpReceiver class in UdpReceiver.cs to handle UDP communication. The UdpReceiver listens on port 11000, processes incoming datagrams, and prints them to the console. Added necessary using directives for networking and asynchronous operations.

--- Navigation Service/Program.cs
+++ Navigation Service/Program.cs
@@ -1,5 +1,9 @@
 ﻿
 using Navigation_Service;
 
-NavigationManager navigationManager = new NavigationManager();
-navigationManager.run();
+//NavigationManager navigationManager = new NavigationManager();
+//navigationManager.run();
+
+UdpReceiver udpReceiver = new UdpReceiver(11000);
+udpReceiver.StartListening();
+while (true) {}
---
+++ Navigation Service/UdpReceiver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Navigation_Service
+{
+    public  class UdpReceiver
+    {
+        private UdpClient _udpClient;
+        private int _port;
+        private IPAddress _IPAddress = IPAddress.Loopback;
+
+        public UdpReceiver(int port = 11000)
+        {
+            _port = port;
+            // socket bound to any IP address on the specified port
+            _udpClient = new UdpClient(new IPEndPoint(_IPAddress, _port));
+            Console.WriteLine($"[UDP Receiver] Bound successfully to {_IPAddress} on port {_port}.");
+        }
+
+        public async Task StartListening()
+        {
+            Console.WriteLine("[UDP Receiver] Listening loop started. Waiting for datagrams..."); 
+            while (true)
+            {
+                // await : recuuve a one datagram !!
+                // while loop : keep receiving datagrams
+                UdpReceiveResult result = await _udpClient.ReceiveAsync();  // UdpReceiveResult is Struct
+                byte[] receivedBytes = result.Buffer; 
+                string receivedText = Encoding.UTF8.GetString(receivedBytes);
+                Console.WriteLine($"Received: {receivedText} from {result.RemoteEndPoint}");
+            }
+        }
+
+        public void StopListening()
+        {
+            _udpClient.Close();
+        }
+
+       
+
+
+    }
+}
